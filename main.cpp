353ad9d04e48c1830c9c2aae9a14f47371574861
CIITSHSDTP00\CIIT <it@ciit.edu.ph>
2025-06-14 15:52:26 +0800

    Completed readCSVToMap frunc

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <vector>
 #include <map>
 #include <cstring>
+#include <fstream>
+#include <sstream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 struct Account {
-	string num;
-	string fname;
-	string mname;
-	string lname;
+	string num, fname, mname, lname;
 	float bal;
 };
 
@@ -17,29 +16,50 @@ string progLoop(); // main program loop; put in main func
 string genAccNum ();
 void memLoad(map<string, Account>* accs);
 void memSave(map<string, Account>* accs);
+void readCSVToMap(string filename, map<string, Account>* accs);
 
 int main() {
 	map<string, Account> accs;
 	
 	memLoad(&accs);
 	
-	cout << accs["101132857923"].fname << "\n";
+	Account acc = accs["111"];
 	
-	memSave(&accs);
+	cout << acc.fname << endl;
 }
 
 void memLoad(map<string, Account>* accs) {
     // read from file code here
-    
-    accs->insert({"101132857922", {"101132857922", "bubu"}});
-	accs->insert({"101132857923", {"101132857922", "baba"}});
-	accs->insert({"101132857924", {"101132857922", "bebe"}});
+	readCSVToMap("accounts.csv", accs);
 }
 
 void memSave(map<string, Account>* accs) {
     // write to file code here
     
-    for (const auto& accPair : *accs) {
-        cout << "Saving " << accPair.first << "...\n";
+    // use this for loop iterator
+    for (map<string, Account>::iterator itr = accs->begin(); itr != accs->end(); itr++) {
+        cout << "Saving " << itr->first << "...\n";
     }
+}
+
+void readCSVToMap(string filename, map<string, Account>* accs) {
+	string line, str, num;
+	ifstream CSV(filename);
+	Account acc;
+	string bal;
+	
+	while (getline(CSV, num, ',')) {
+		acc = Account();
+		acc.num = num;
+		getline(CSV, acc.fname, ',');
+		getline(CSV, acc.mname, ',');
+		getline(CSV, acc.lname, ',');
+		getline(CSV, bal);
+		acc.bal = stof(bal);
+		accs->insert(make_pair(num, acc));
+		num = "";
+		bal = "";
+	}
+	
+	CSV.close();
 }

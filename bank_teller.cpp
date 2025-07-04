910e87692c737ca43e2a1645b77f5ec82af69aa7
astralicht <eneioarzew@gmail.com>
2025-07-05 02:13:17 +0800

    Updates

--- bank_teller.cpp
+++ bank_teller.cpp
@@ -1,12 +1,30 @@
+/*
+
+To do list:
+- customer details not showing when checking account details
+
+*/
+
+/*
+CS105-1
+
+Julac Ontina
+Jericho Capistrano
+Ricoh Ng
+*/
+
 #include <iostream>
 #include <map>
 #include <cstring>
+#include <cctype>
 #include <fstream>
-#include <sstream>
 #include <conio.h>
 
-using namespace std;
+#define ASCII_ESC 27
+#define ASCII_ENTER 13
+#define ASCII_BACKSPACE 8
 
+using namespace std;
 
 struct Customer {
 	string cusNum, fname, mname, lname, phone_number, email;
@@ -37,10 +55,12 @@ map<string, Account> acs;
 map<string, Transaction> trs;
 
 int main() {
-	memLoad("customers.csv", "customers");
-	memLoad("accounts.csv", "accounts");
-	memLoad("transactions.csv", "transactions");
-	progLoop();
+	memLoad("csv\\customers.csv", "customers");
+	memLoad("csv\\accounts.csv", "accounts");
+	memLoad("csv\\transactions.csv", "transactions");
+	
+	cout << css["111"].fname;
+//	progLoop();
 }
 
 void progLoop() {
@@ -49,7 +69,7 @@ void progLoop() {
 	while(true) {
 		system("cls");
 		header();
-		cout << "\nPress any key to start.\n";
+		cout << "Press any key to start.";
 		_getch();
 		
 		while(true) {
@@ -57,49 +77,82 @@ void progLoop() {
 			header();
 			cout << "[1] View Account\n";
 			cout << "[2] Open New Account\n";
-			cout << "[0] Cancel\n";
+			cout << "[3] View Customer Profile\n";
+			cout << "[4] Create New Customer\n";
+			cout << "[5] Update Customer Profile\n";
+			cout << "[6] Withdrawal\n";
+			cout << "[7] Deposit\n";
+			cout << "[ESC] Cancel\n";
+			cout << "> ";
 			inp = _getch();
+			
+			if (inp == ASCII_ESC) break;
 			
 			system("cls");
 			
 			if (inp == '1') {
 				viewAccount();
-			} else {
-				continue;
-			}
+			} else continue;
 		}
 	}
 }
 
 void header() {
-	cout << "=====           Three Blind Mice Bank           =====\n";
+	cout << "=====           Three Blind Mice Bank           =====\n\n";
 }
 
 void viewAccount() {
 	string input;
+	char charInp;
 	Account acc;
+	Customer cus;
 	
 	while (true) {
 		system("cls");
+		input = "";
+		charInp = '\0';
 		header();
-		cout << "Enter account number";
-		cout << "> ";
-		cin >> input;
-		acc = fetchAccount(input);
+		cout << "Enter account number.\nEnter to confirm. ESC to cancel.\n> ";
+		
+		while (true) {
+			charInp = _getch();
+			if (charInp == ASCII_ESC) return;
+			else if (charInp == ASCII_ENTER) break;
+			else if (charInp == ASCII_BACKSPACE) {
+				system("cls");
+				header();
+				cout << "Enter account number.\nEnter to confirm. ESC to cancel.\n> ";
+				if (input.length() > 0) input.erase(input.length()-1, 1);
+				cout << input;
+			}
+			else {
+				input += charInp;
+				cout << charInp;
+			}
+		}
+		
+		acc = acs[input];
 		
 		if (acc.accNum == "") {
 			cout << "\nAccount not found.\nPress any key to try again.\n";
 			_getch();
 			continue;
-		} else {
-			break;
-		}
+		} else break;
 	}
+	
+	cus = css[acc.cusNum];
 	
 	while (true) {
 		system("cls");
 		header();
-		
+		cout << "Account Number: " << acc.accNum << endl;
+		cout << "Customer Name: " << cus.lname << ", " << cus.fname << " " << cus.mname << endl;
+		cout << "\n---------------\n";
+		cout << "Balance\n";
+		cout << "---------------\n";
+		cout << "Php " << acc.balance << endl;
+		cout << "---------------\n";
+		_getch();
 	}
 }
 
@@ -109,9 +162,11 @@ void memLoad(string filepath, string tableName) {
 	
 	if (!ifs) {
 		system("cls");
-		cout << "System error!";
-		cout << "Please contact the system administrator to resolve the issue.";
+		header();
+		cout << "System error!\n";
+		cout << "Please contact the system administrator to resolve the issue.\n";
 		cout << "The program will exit now.";
+		_getch();
 		exit(0);
 	}
 	
@@ -126,7 +181,7 @@ void memLoad(string filepath, string tableName) {
 			getline(ifs, cus.lname, ',');
 			getline(ifs, cus.phone_number, ',');
 			getline(ifs, cus.email, ',');
-			css.insert(make_pair(cusNum, cus));
+			css[cusNum] = cus;
 		}
 	} else if (tableName == "accounts") {
 		Account acc;
@@ -137,30 +192,17 @@ void memLoad(string filepath, string tableName) {
 			getline(ifs, acc.cusNum, ',');
 			getline(ifs, balance);
 			acc.balance = stof(balance);
+			acs[accNum] = acc;
 		}
 	}
 	
 	ifs.close();
 }
 
-//void memSave(string filename, string tableName) {
-//    // write to file code here
-//    
-//    // use this for loop iterator
-//    for (map<string, Customer>::iterator itr = css.begin(); itr != css.end(); itr++) {
-//        cout << "Saving " << itr->first << "...\n";
-//    }
-//}
-
-Account fetchAccount(string accNum) {
-	for (map<string, Account>::iterator itr = acs.begin(); itr != acs.end(); itr++) {
-        if (itr->second.accNum == accNum) {
-        	return itr->second;
-		}
-    }
+void memSave(string filename, string tableName) {
+    // write to file code here
 }
 
 //string genAccNum() {
 //	// code here to generate account number for new customer
 //}
-
